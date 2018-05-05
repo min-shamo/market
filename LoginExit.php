116394ebfbf94664d6aea002bf6be051bfb949b6
shamo <990041251@qq.com>
2018-05-05 23:33:27 +0800

    Revert "毕设"
    This reverts commit 574f2cf58f80a34a79fd6e1d507908eab6cd3e4e.

--- LoginExit.php
+++
@@ -1,6 +0,0 @@
-<?PHP
-  session_start();
-  $_SESSION["user_id"]="";
-  $_SESSION["user_pwd"]="";
-  header("Location: "."index.php");
-?>
--- admin/logout.php
+++
@@ -1,8 +0,0 @@
-<?PHP
-    session_start();
-    unset($_SESSION['user_id']);
-    unset($_SESSION['user_pwd']);
-    unset($_SESSION['user_type']);
-
-    header("Location: ../index.php");
-?>
